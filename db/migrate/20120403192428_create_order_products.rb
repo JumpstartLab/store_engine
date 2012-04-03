42f883e77a26b7d78151e99cec5aa425c1f366bb
Melanie Gilman <melanie.gilman@gmail.com>
2012-04-03 19:20:46 -0400

    Removed order product

--- db/migrate/20120403192428_create_order_products.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrderProducts < ActiveRecord::Migration
-  def change
-    create_table :order_products do |t|
-      t.integer :quantity
-      t.integer :price
-      t.timestamps
-    end
-  end
-end
