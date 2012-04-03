665446bc06da15e14b52bb7d392a877595a1b278
Chris Anderson and Tom Kiefhaber <christopher.anderson+chrismanderson+tkiefhaber@gmail.com>
2012-04-03 14:55:43 -0400

    added todo list and some product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :description, :activity, :price, :image_link
+
+  validates_presence_of :title
+  validates_presence_of  :description
+  validates_presence_of :price
+
+  validates_uniqueness_of :title
 end
