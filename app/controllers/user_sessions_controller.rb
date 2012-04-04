b4a30d367643093417822bceb5c597df53d5a1e9
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-04-04 17:34:07 -0400

    Clarify temporary variable

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    cart_id = current_cart.id
+    persisting_cart_id = current_cart.id
     user = login(params[:email], params[:password])
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
@@ -12,7 +12,7 @@ class UserSessionsController < ApplicationController
       flash.now.alert = "Email or password was invalid."
       redirect_to :login
     end
-    session[:cart_id] = cart_id
+    session[:cart_id] = persisting_cart_id
   end
 
   
