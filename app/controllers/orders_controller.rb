0d322495a3e08ed0d1bc7531fa90af78296fc623
Chris Anderson <christopher.anderson@gmail.com>
2012-04-04 17:43:21 -0400

    Order dropdown form is crazy cool

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,20 @@
 class OrdersController < ApplicationController
+  
   def index
     @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update_attributes(params[:order])
+    redirect_to order_path(@order)
   end
 end
