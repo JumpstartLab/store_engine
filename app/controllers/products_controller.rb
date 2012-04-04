cfb03cdf84d30ebe81c3b829fdab31463b46380a
Chris Anderson and Tom Kiefhaber <christopher.anderson+chrismanderson+tkiefhaber@gmail.com>
2012-04-04 15:37:54 -0400

    product page beginning with baller profile pic

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(params[:product])
+    product.save
+    redirect_to products_path
+  end
+
+  #   def new
+  #   @article = Article.new
+  # end
+
+  # def create
+  #   article = Article.new(params[:article])
+  #   article.save
+  #   redirect_to article_path(article)
+  # end
+end
---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ProductsController do
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+  end
+end
