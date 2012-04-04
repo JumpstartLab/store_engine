deb210f2ba24983e4b3b9cffb53d97dd35b95305
Travis Valentine and Horace Williams <tvalent2+travisvalentine+worace@gmualumni.org>
2012-04-03 23:40:03 -0400

    Add first and last name fields to signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Email or password was invalid"
+      flash.now.alert = "Invalid login/password."
       render :new
     end
   end
