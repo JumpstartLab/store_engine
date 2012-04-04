8ff182c1da2a394a017b9de1cc582bd4e8d5d6c8
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-04-04 17:29:22 -0400

    Persist cart after user logs in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,6 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
+    cart_id = current_cart.id
     user = login(params[:email], params[:password])
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
@@ -11,6 +12,7 @@ class UserSessionsController < ApplicationController
       flash.now.alert = "Email or password was invalid."
       redirect_to :login
     end
+    session[:cart_id] = cart_id
   end
 
   
