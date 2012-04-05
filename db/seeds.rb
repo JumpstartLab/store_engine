989c6ca27fda1744e2cede165898a2abdff2473c
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-04-05 14:06:24 -0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 
 c1 = Category.create(name: "Great Stuff")
 c2 = Category.create(name: "Pretty Good Stuff")
-p1 = Product.create(title: "Sample Product 1", description: "Lorem ipsum dolor sit amet", category_id: c1.id price_in_cents: 1050)
-p2 = Product.create(title: "Second Product Sample", description: "Foo bar baz quux", category_id: c2.id, price_in_cents: 19999)
-u = User.create(full_name: "Mark Tabler", email: "a@b.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-u = User.create(full_name: "Charles Strahan", email: "b@c.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-
+p1 = Product.create(name: "Sample Product 1", description: "Lorem ipsum dolor sit amet", category_id: c1.id, price_in_cents: 1050)
+p2 = Product.create(name: "Second Product Sample", description: "Foo bar baz quux", category_id: c2.id, price_in_cents: 19999)
+u1 = User.create(full_name: "Mark Tabler", email: "a@b.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+u2 = User.create(full_name: "Charles Strahan", email: "b@c.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+u3 = User.create(full_name: "Happy Admin", email: "admin@store.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+u3.update_attribute(:admin, true)
