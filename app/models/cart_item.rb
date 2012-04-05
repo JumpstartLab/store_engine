b3c76c8624e7c693c542a5bf79473dd6a669d6c9
ADigitalNative <adigitalnative@gmail.com>
2012-04-05 18:22:41 -0400

    Added Stuff

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,5 @@
 class CartItem < ActiveRecord::Base
-  attr_accessible :id, :product_id, :cart_id
+  # attr_accessible :id, :product_id, :cart_id
 
   belongs_to :cart
   belongs_to :product
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Cart do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:cart) { Cart.create }
+  let(:product) { Fabricate(:product) }
+  let (:products) { [product, Fabricate(:product)] }
+
+  describe "#items" do
+    context "when items have been added to the cart" do
+      before(:each) { products.each { |p| cart.add_item(p) } }
+
+      it "returns the items" do
+        cart.items.count.should == products.count
+        products.each do |p|
+          cart.items.should include(p)
+        end
+      end
+    end
+  end
+
+  describe "#add_product_by_id" do
+    it "adds a product to the cart" do
+      expect {
+        cart.add_product_by_id(product.id)
+      }.to change { cart.items.count }.by(1)
+    end
+  end
 end
