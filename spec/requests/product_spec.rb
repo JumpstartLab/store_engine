43a31d585e94044b746eab3b881361d82d4971e6
Mike Chlipala <mchlipala@gmail.com>
2012-04-08 12:24:30 -0400

    Tests for product requests

---
+++ spec/requests/product_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Updating products" do
+  context "when I'm on a product page" do
+    let(:product) { FactoryGirl.create(:product) }
+    before(:each) { visit product_path(product)}
+
+    context "and I click 'Edit this product'" do
+      before(:each) { click_link("Edit this product") }
+
+      it "takes me a product's edit page" do
+        page.should have_content("Edit this product")
+      end
+
+      context "and I update the product" do
+        before(:each) { click_button("Update Product") }
+        it "takes me back to the product's detail page" do
+          page.should have_content(product.name)
+        end
+
+        it "lets me know the product was updated" do
+          page.should have_content("Product was successfully updated.")
+        end
+      end
+
+      context "and I click 'Back'" do
+        before(:each) { click_link("Back")}
+
+        it "takes me back to the list of products" do
+          page.should have_content("Products")
+        end
+      end
+    end
+  end
+end
