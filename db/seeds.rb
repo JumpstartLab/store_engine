1db2e451c9303d626456b26410a8a965281d65fb
Travis Valentine <travis.valentine@livingsocial.com>
2012-04-08 21:33:35 -0400

    Fix seed file and css image height

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 matt = User.create(:first_name => "Matt", :last_name => "Yoho",
-      :email => "matt.yoho@livingsocial.com", :password => "hungry", :admin => true )
+      :email => "matt.yoho@livingsocial.com", :password => "hungry" )
 
 jeff = User.create(:first_name => "Jeff", :last_name => "",
       :email => "jeff.casimir@livingsocial.com", :password => "hungry", :display_name => "j3")
 
 chad = User.create(:first_name => "Chad", :last_name => "Fowler",
-      :email => "chad.fowler@livingsocial.com", :password => "hungry", :display_name => "SaxPlayer")
+      :email => "chad.fowler@livingsocial.com", :password => "hungry", :display_name => "SaxPlayer", :admin => true)
 
 
 chair = Product.create(:title => "Sweet chair", :description => "This is a great chair", :price => "5.00", :remote_image_url => "http://ecx.images-amazon.com/images/I/31pVwh3qNvL._SL500_AA300_.jpg")
 
-table = Product.create(:title => "Great table", :description => "This is the sweetest chair", :price => "10.00", :remote_image_url => "http://ecx.images-amazon.com/images/I/31lRJMSUDOL._SL500_AA300_.jpg")
+table = Product.create(:title => "Great table", :description => "This is the sweetest table", :price => "10.00", :remote_image_url => "http://ecx.images-amazon.com/images/I/31lRJMSUDOL._SL500_AA300_.jpg")
 
 desk = Product.create(:title => "Amazing desk", :description => "This is the coolest desk", :price => "20.00", :remote_image_url => "http://www.vintagedanishmodern.com/products/87381.jpg")
