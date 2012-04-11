47fec05d2bd9cd1fd2bac22af15b6ba486a6ef3c
Melanie Gilman <melanie.gilman@gmail.com>
2012-04-11 18:43:10 -0400

    Cart can be edited yay

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -5,7 +5,10 @@ class ShoppingCartController < ApplicationController
   end
 
   def update
-    cart.add_item(params[:product], params[:cart_item][:quantity])
+    cart.cart_items.each do |cart_item|
+      cart_item.quantity = params[:quantity][cart_item.id.to_s]
+      cart_item.save
+    end
     redirect_to shopping_cart_path
   end
 end
--- spec/requests/shopping_cart_requests_spec.rb
+++ spec/requests/shopping_cart_requests_spec.rb
@@ -11,14 +11,14 @@ describe "Shopping Cart Requests" do
 
   
   context "when I visit the shopping cart" do
-    it "should show the logged in users' cart items " do
+    it "shows the logged in users' cart items " do
       user.shopping_cart = Fabricate(:shopping_cart)
       user.shopping_cart.add_item(product.id, 10)
       visit shopping_cart_path
       find_link(product.title).visible? 
     end
 
-    it "should allow me to remove an item from my cart" do
+    it "allows me to remove an item from my cart" do
       user.shopping_cart = Fabricate(:shopping_cart)
       user.shopping_cart.add_item(product.id, 10)
       user.shopping_cart.add_item(product2.id, 20)
@@ -29,13 +29,33 @@ describe "Shopping Cart Requests" do
     end
   end
 
-  context "when I add an item to the cart" do
-    it "should show the newly created item in the cart" do
-      params = { :product => product.id, :cart_item => {:quantity => 10} }
-      page.driver.put(shopping_cart_path(product), params) 
+  context "When I update the shopping cart" do
+    let(:cart) { Fabricate(:shopping_cart) }
+
+    before(:each) do
+      user.shopping_cart = cart
+      cart.add_item(product.id, 10)
+    end
+    let(:text_field_id) { "quantity[#{cart.cart_items.first.id}]" }
+
+    it "changes the quantity of an item" do
       visit shopping_cart_path
-      find_link(product.title).visible?
+      
+      fill_in text_field_id, :with => 3
+      click_button("Update Shopping cart")
+      find_field(text_field_id).value.should == "3"
+    end
+
+    it "changes the quantity of multiple item" do
+      cart.add_item(product2.id, 50)
+      visit shopping_cart_path
+      
+      text_field_id2 = "quantity[#{cart.cart_items.second.id}]"
+      fill_in text_field_id, :with => 3
+      fill_in text_field_id2, :with => 9
+      click_button("Update Shopping cart")
+      find_field(text_field_id).value.should == "3"
+      find_field(text_field_id2).value.should == "9"
     end
   end
-
 end
