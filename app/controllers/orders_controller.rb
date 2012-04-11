d1995a966ae6ef03a48a4c5feb99041e9bdb412d
Edward Weng <edweng@gmail.com>
2012-04-11 14:55:24 -0400

    Refactored order code.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new
+    @order = Order.new(:user_id => current_user, :status => 'pending')
     @order.stripe_card_token = params[:order][:stripe_card_token]
     if @order.save_customer
-      redirect_to @order, :notice => "Thanks a whole bunch."
+      if @order.charge(current_cart.cart_total_in_cents)
+        redirect_to @order, :notice => "Thank you for placing an order."
+      end
     else
       render :new
     end
