ab497773d1c705387b30b21eb26643ae78c020d4
Chris Anderson <christopher.anderson@gmail.com>
2012-04-12 10:03:42 -0400

    redirect bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,14 @@ class SessionsController < ApplicationController
     user = lookup_by_email_or_username(params[:account_name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session[:request_page], notice: "Logged in!" unless session[:request_page].blank?
-      redirect_to root_url
+      if session[:request_page].blank?
+        redirect_to root_url
+      else
+        new_session = session[:request_page]
+        session[:request_page] = nil
+        redirect_to new_session, notice: "Logged in!"
+      end
+      
     else
       flash.now.alert = "Email or password is invalid."
       render "new"
