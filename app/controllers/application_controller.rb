13d297cc37fbfc06f15a748d3062e5f11634fff7
Jan Koszewski <jan.koszewski@gmail.com>
2012-04-12 13:55:05 -0400

    attempted fix for cart blakning on full table drop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,17 @@ class ApplicationController < ActionController::Base
 
   def find_or_create_cart
     if session[:cart_id]
-      Cart.find_by_id(session[:cart_id])
+      cart_finder
     else
       Cart.create.tap{ |c| session[:cart_id] = c.id }
+    end
+  end
+
+  def cart_finder
+    if Cart.find_by_id(session[:cart_id]).nil?
+      Cart.create.tap{ |c| session[:cart_id] = c.id }
+    else 
+      Cart.find_by_id(session[:cart_id])
     end
   end
 end
