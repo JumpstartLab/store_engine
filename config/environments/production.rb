d300c6af3ce685a704d9f11b3cdc5c78384ebea2
Travis Valentine and Horace Williams <tvalent2+travisvalentine+worace@gmualumni.org>
2012-04-13 18:51:02 -0400

    Heroku fix?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,4 @@ StoreEngine::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # Heroku error fix?
-  # ::ActiveSupport::Deprecation.silenced = true
 end
