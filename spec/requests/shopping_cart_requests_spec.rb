71b8833eb49912e5f5bbc4834ff655f5a06033be
Austen Ito <austen.services@gmail.com>
2012-04-14 17:19:26 -0400

    Fixed seed data and removed checkout option when not logged in

--- spec/requests/shopping_cart_requests_spec.rb
+++ spec/requests/shopping_cart_requests_spec.rb
@@ -5,27 +5,44 @@ describe "Shopping Cart Requests" do
   let!(:product) { Fabricate(:product, :title => "iPod") }
   let!(:product2) { Fabricate(:product, :title => "Macbook Pro") }
 
-  before(:each) do
-    login_user_post("admin", "admin")
-  end
 
-  
   context "when I visit the shopping cart" do
-    it "shows the logged in users' cart items " do
-      user.shopping_cart = Fabricate(:shopping_cart)
-      user.shopping_cart.add_item(product.id, 10)
-      visit shopping_cart_path
-      find_link(product.title).visible? 
+    context "and not logged in" do
+      it "does not allow checkout" do
+        visit shopping_cart_path
+        page.should_not have_content("Checkout")
+      end
     end
 
-    it "allows me to remove an item from my cart" do
-      user.shopping_cart = Fabricate(:shopping_cart)
-      user.shopping_cart.add_item(product.id, 10)
-      user.shopping_cart.add_item(product2.id, 20)
-      visit shopping_cart_path
-      click_link("Remove")
-      page.should_not have_content(product.title)
-      page.should have_content(product2.title)
+    context "and logged in" do
+      before(:each) do
+        login_user_post("admin", "admin")
+      end
+
+      it "shows the logged in users' cart items " do
+        user.shopping_cart = Fabricate(:shopping_cart)
+        user.shopping_cart.add_item(product.id, 10)
+        visit shopping_cart_path
+        find_link(product.title).visible? 
+      end
+
+      it "allows me to remove an item from my cart" do
+        user.shopping_cart = Fabricate(:shopping_cart)
+        user.shopping_cart.add_item(product.id, 10)
+        user.shopping_cart.add_item(product2.id, 20)
+        visit shopping_cart_path
+        click_link("Remove")
+        page.should_not have_content(product.title)
+        page.should have_content(product2.title)
+      end
+
+      context "When I check out" do
+        it "brings me to the shipping address page" do
+          visit shopping_cart_path
+          click_link("Checkout")
+          current_path.should == new_shipping_address_path
+        end
+      end
     end
   end
 
@@ -33,6 +50,7 @@ describe "Shopping Cart Requests" do
     let(:cart) { Fabricate(:shopping_cart) }
 
     before(:each) do
+      login_user_post("admin", "admin")
       user.shopping_cart = cart
       cart.add_item(product.id, 10)
     end
@@ -40,7 +58,7 @@ describe "Shopping Cart Requests" do
 
     it "changes the quantity of an item" do
       visit shopping_cart_path
-      
+
       fill_in text_field_id, :with => 3
       click_button("Update Shopping cart")
       find_field(text_field_id).value.should == "3"
@@ -49,7 +67,7 @@ describe "Shopping Cart Requests" do
     it "changes the quantity of multiple item" do
       cart.add_item(product2.id, 50)
       visit shopping_cart_path
-      
+
       text_field_id2 = "quantity[#{cart.cart_items.second.id}]"
       fill_in text_field_id, :with => 3
       fill_in text_field_id2, :with => 9
@@ -59,11 +77,4 @@ describe "Shopping Cart Requests" do
     end
   end
 
-  context "When I check out" do
-    it "brings me to the shipping address page" do
-      visit shopping_cart_path
-      click_link("Checkout")
-      current_path.should == new_shipping_address_path
-    end
-  end
 end
