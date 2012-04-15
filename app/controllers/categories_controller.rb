767c52f2309a6e33a0f62da9f136fb6fc4f85c91
Elise Worthy <elise.worthy@livingsocial.com>
2012-04-15 13:23:36 -0400

    removed categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,6 @@
 class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
-  def index
-    @categories = Category.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @categories }
-    end
-  end
 
   def show
     @category = Category.find(params[:id])
