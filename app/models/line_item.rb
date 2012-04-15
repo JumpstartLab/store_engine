e285fb1011fe969425e7a1fd01d9225343dd2959
Elise Worthy <elise.worthy@livingsocial.com>
2012-04-15 09:13:31 -0400

    fixed line item - no need for @subtotal

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,10 +2,9 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
   has_one :order_item
-  #attr_accessible :cart_id, :product_id, :product, :subtotal, :quantity, :price, :id
   attr_accessible :product_id, :product
 
   def subtotal
-    @subtotal ||= self.product.price * quantity
+    self.product.price * quantity
   end
 end
