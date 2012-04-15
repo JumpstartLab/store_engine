93c65d7b588a79123b3ee18cd24cf197d0fe89e9
Melanie Gilman <melanie.gilman@gmail.com>
2012-04-15 15:07:40 -0400

    Added filter for categories

--- app/controllers/categories_products_controller.rb
+++ app/controllers/categories_products_controller.rb
@@ -2,5 +2,6 @@ class CategoriesProductsController < ApplicationController
 
   def index
     @category = Category.find(params[:category_id])
+    @products = Category.find(params[:category_id]).products.find(:all, :conditions => { :retired => false})
   end
 end
