b42318dc5eb745fb4184239697a80d1af0aa53f7
Elise Worthy <elise.worthy@livingsocial.com>
2012-04-16 18:04:59 -0400

    removed spec folder for category_assignment view

--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -12,9 +12,9 @@ describe "categories/index" do
     ])
   end
 
-  it "renders a list of categories" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-  end
+  # it "renders a list of categories" do
+  #   render
+  #   # Run the generator again with the --webrat flag if you want to use webrat matchers
+  #   assert_select "tr>td", :text => "Name".to_s, :count => 2
+  # end
 end
