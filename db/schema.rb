e21bd55b71be1af0ac99351d342ea18c82945c08
Chris Anderson <christopher.anderson@gmail.com>
2012-04-15 22:18:10 -0400

    added title for address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120414202643) do
+ActiveRecord::Schema.define(:version => 20120416021736) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street_1"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20120414202643) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
   create_table "cart_items", :force => true do |t|
