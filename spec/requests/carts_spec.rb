3c729be0b02c7223da4cdfcb964a3a7eec8e0823
Elise Worthy <elise.worthy@livingsocial.com>
2012-04-17 19:00:32 -0400

    added integration spec files

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,11 +1,11 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe "Carts" do
-#   describe "GET /cart" do
-#     let(:cart) {FactoryGirl.build(:cart)}
-#     pending it "works! (now write some real specs)" do
-#       get cart_path(cart)
-#       response.status.should be(200)
-#     end
-#   end
-# end
+describe "Carts" do
+  describe "GET /cart" do
+    let(:cart) {FactoryGirl.build(:cart)}
+    pending it "works! (now write some real specs)" do
+      get cart_path(cart)
+      response.status.should be(200)
+    end
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "Categories" do
-  describe "GET /categories" do
+describe "Users" do
+  describe "GET /users" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get categories_path
+      get users_index_path
       response.status.should be(200)
     end
   end
