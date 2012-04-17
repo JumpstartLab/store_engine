155299392405aafe351b7b59ce42bb4cbcce163a
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-04-17 11:13:42 -0400

    everything but admin interface for sale finished

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,7 +4,9 @@ class Sale < ActiveRecord::Base
   has_many :categories
   has_many :products
 
-  validates_presence_of :percent_off
+  validates_presence_of :percent_off, :end_at
+
+  default_scope where("end_at > ?", Time.now)
 
   def percent
     self.percent_off.to_f / 100
