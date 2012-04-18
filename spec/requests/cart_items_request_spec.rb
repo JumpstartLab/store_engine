5896d38bee1e900913a4848c170c5679168c0716
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-04-17 22:27:56 -0400

    Finish cart_items spec

--- spec/requests/cart_items_request_spec.rb
+++ spec/requests/cart_items_request_spec.rb
@@ -36,4 +36,17 @@ require 'spec_helper'
         page.should have_content "3"
       end
     end
+
+    it "allows a user to delete a cart item" do
+      u = Fabricate(:user)
+      p = Fabricate(:product)
+      log_in(u, 'asdfasdf')
+      visit product_path(p)
+      click_button "Add Item To Cart"
+      click_link "x"
+      within "#mini_cart" do
+        page.should_not have_content p.name
+      end
+    end
+
   end
