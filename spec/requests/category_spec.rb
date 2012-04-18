076b7c905dac220f91f041f4c29939b03ff3160c
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-04-17 23:16:15 -0400

    good looking dashboard

---
+++ spec/requests/category_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "sorting by categories" do
+  describe "when user clicks on a category link" do
+    it "redirects to category product page"
+      visit "/"
+    end
+  end
+end
