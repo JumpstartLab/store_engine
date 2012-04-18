d3316ffb4e35ddd03f13d3c7072ca59077bf87f9
Melanie Gilman <melanie.gilman@gmail.com>
2012-04-18 17:21:16 -0400

    Removed fabrications from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,28 +12,28 @@ Category.create(:name => "awesome")
 Category.create(:name => "not as awesome")
 Category.create(:name => "pretty terrible")
 
-30.times { Fabricate(:product).save } 
-
-pending = Status.create(:name => StoreEngine::Status::PENDING)
-Status.create(:name => StoreEngine::Status::CANCELLED)
-paid = Status.create(:name => StoreEngine::Status::PAID)
-shipped = Status.create(:name => StoreEngine::Status::SHIPPED)
-Status.create(:name => StoreEngine::Status::RETURNED)
-
-shipping_address = Fabricate(:shipping_address)
-shipping_address.save
-billing_address = Fabricate(:billing_address)
-billing_address.save
-
-item = OrderItem.new(:product => Product.find(1))
-item2 = OrderItem.new(:product => Product.find(3))
-items = [item, item2]
-Order.create(:user => matt, :status => pending, :order_items => items,
-                    :shipping_address => shipping_address, 
-                    :billing_address => billing_address)
-Order.create(:user => jeff, :status => shipped,
-                    :shipping_address => shipping_address, 
-                    :billing_address => billing_address).save
-Order.create(:user => jeff, :status => paid,
-                    :shipping_address => shipping_address, 
-                    :billing_address => billing_address).save
+# 30.times { Fabricate(:product).save } 
+# 
+# pending = Status.create(:name => StoreEngine::Status::PENDING)
+# Status.create(:name => StoreEngine::Status::CANCELLED)
+# paid = Status.create(:name => StoreEngine::Status::PAID)
+# shipped = Status.create(:name => StoreEngine::Status::SHIPPED)
+# Status.create(:name => StoreEngine::Status::RETURNED)
+# 
+# shipping_address = Fabricate(:shipping_address)
+# shipping_address.save
+# billing_address = Fabricate(:billing_address)
+# billing_address.save
+# 
+# item = OrderItem.new(:product => Product.find(1))
+# item2 = OrderItem.new(:product => Product.find(3))
+# items = [item, item2]
+# Order.create(:user => matt, :status => pending, :order_items => items,
+#                     :shipping_address => shipping_address, 
+#                     :billing_address => billing_address)
+# Order.create(:user => jeff, :status => shipped,
+#                     :shipping_address => shipping_address, 
+#                     :billing_address => billing_address).save
+# Order.create(:user => jeff, :status => paid,
+#                     :shipping_address => shipping_address, 
+#                     :billing_address => billing_address).save
