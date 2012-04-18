9c5a99bf1c81685fe1c1aba1430c004ec0a55067
Andrew Thal <andrew.thal@livingocial.com>
2012-04-18 18:25:33 -0400

    try again for heroku

--- db/migrate/20120417010840_change_data_type_for_credit_card_expiration_date.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDataTypeForCreditCardExpirationDate < ActiveRecord::Migration
-  def change
-    change_table :billing_methods do |t|
-      t.change :credit_card_expiration_date, :datetime
-    end
-  end
-end
