0e4296693c0684c1a328cb0a3efd4fe5e118ee48
Charles Strahan <charles.c.strahan@gmail.com>
2012-04-18 10:47:46 -0400

    Make routes match specs.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ StoreEngine::Application.routes.draw do
 
   resources :categories
 
-  resources :products do
+  resources :products, :except => :index do
     post "two_click_order", on: :member
   end
 
@@ -96,7 +96,7 @@ StoreEngine::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   match "/code" => "pages#code"
-  root :to => 'pages#home'
+  root :to => 'products#index'
 
   # See how all your routes lay out with "rake routes"
 
