ec0ee4c6a424091c266db7382e3ce98fb00da786
Mike Chlipala <mchlipala@gmail.com>
2012-04-19 14:59:53 -0400

    turning off twilio

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -13,8 +13,8 @@ class CallsController < ApplicationController
   def new
     @client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     @account = @client.account
-    @call = @account.calls.create({:from => '5866904041',
-      :to => '5862914303', :url => calls_url })
+    # @call = @account.calls.create({:from => '5866904041',
+    #   :to => '5862914303', :url => calls_url })
     puts @message
     redirect_to calls_url
   end
