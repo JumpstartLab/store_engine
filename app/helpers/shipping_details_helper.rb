24005df0dabae0d58fdab0443a62c6a6a31c3c2d
Edward Weng <edweng@gmail.com>
2012-04-19 01:34:41 -0400

    Fixed specs for helper

--- app/helpers/shipping_details_helper.rb
+++ app/helpers/shipping_details_helper.rb
@@ -1,12 +1,12 @@
 module ShippingDetailsHelper
 
-  def formatted_shipping_address
-    "#{ @shipping_detail.ship_to_name }<br />
-    #{ @shipping_detail.ship_to_address_1 }<br />
-    #{ @shipping_detail.ship_to_address_2 }<br />
-    #{ @shipping_detail.ship_to_city }, 
-    #{ @shipping_detail.ship_to_state } 
-    #{ @shipping_detail.ship_to_zip }"
+  def formatted_shipping_address(shipping_detail)
+    "#{ shipping_detail.ship_to_name }<br />
+    #{ shipping_detail.ship_to_address_1 }<br />
+    #{ shipping_detail.ship_to_address_2 }<br />
+    #{ shipping_detail.ship_to_city }, 
+    #{ shipping_detail.ship_to_state } 
+    #{ shipping_detail.ship_to_zip }"
   end
 
   def us_states
