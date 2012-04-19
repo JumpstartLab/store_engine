9589ca1b5fa3a50d4874823b1965db3d4296dfd7
Austen Ito <austen.services@gmail.com>
2012-04-18 21:12:46 -0400

    Added email validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
-  validates :email, :uniqueness => true
+  validates :email, :uniqueness => true, :email => true
   validates :first_name, :last_name, :email, :presence => true 
   validates :display_name, :allow_blank => true, :length => { :in => 2..32 }
   has_one :shopping_cart
