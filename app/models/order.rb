b9f76054fe92d8117d48315b88be9c6df36ca236
Elise Worthy <elise.worthy@livingsocial.com>
2012-04-19 10:48:43 -0400

    fixed order model for reek

--- app/models/order.rb
+++ app/models/order.rb
@@ -24,22 +24,40 @@ class Order < ActiveRecord::Base
   end
 
   def self.one_click_order(product, user)
-    order_item = OrderItem.new
-    order = Order.new(user: user)
-    order_item.set_price_from_product(product)
-    order_item.product = product
-    order_item.quantity = 1
-    order_item.order = order
-    order_item.save
-    order.name = user.orders.last.name
-    order.email = user.orders.last.email
-    order.address = user.orders.last.address
-    order.cc_number = user.orders.last.cc_number
-    order.cc_expiry = user.orders.last.cc_expiry
-    order.save
-
-    order
+    @user = user
+    @product = product
+    self.one_click_create_order_item
+    self.one_click_create_order
+    @order
   end
+
+  def self.one_click_create_order_item
+    @order_item = OrderItem.new
+    self.set_order_item_info
+    @order_item.save
+  end
+
+  def self.one_click_create_order
+    @order = Order.new(user: @user)
+    self.set_order_info
+    @order.save
+  end
+
+  def self.set_order_item_info
+    @order_item.set_price_from_product(@product)
+    @order_item.product = @product
+    @order_item.quantity = 1
+    @order_item.order = @order
+  end
+
+  def self.set_order_info
+    @order.name = @user.orders.last.name
+    @order.email = @user.orders.last.email
+    @order.address = @user.orders.last.address
+    @order.cc_number = @user.orders.last.cc_number
+    @order.cc_expiry = @user.orders.last.cc_expiry
+  end
+
 
   def ship
     update_attributes(:shipped_at => Time.now, :status => "shipped")
@@ -54,20 +72,26 @@ class Order < ActiveRecord::Base
   end
 
   def add_contents_of_cart(cart, order)
-    cart.line_items.each do |line_item|
-      order_item = OrderItem.new
-      order_item.set_price(line_item)
-      order_item.set_product(line_item)
-      order_item.set_quantity(line_item)
-      order_item.order_id = order.id
-      order_item.save
+    @cart = cart
+    @order = order
+    @cart.line_items.each do |line_item|
+      create_order_item(line_item)
     end
-  order.order_items
+  @order.order_items
   end
 
-  # def total
-  #   line_items.sum(@line_item.total_price)
-  # end
+  def create_order_item(line_item)
+    order_item = OrderItem.new
+    set_order_item_info(line_item, order_item)
+    order_item.save
+  end
+
+  def set_order_item_info(line_item, order_item)
+    order_item.set_price(line_item)
+    order_item.set_product(line_item)
+    order_item.set_quantity(line_item)
+    order_item.order_id = @order.id
+  end
 
   def total
     total = 0
