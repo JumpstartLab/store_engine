2e224ab07bcb327fa7d74ff9169ed3e8f807da96
Elise Worthy <elise.worthy@livingsocial.com>
2012-04-19 10:20:40 -0400

    sessions controller changes for reek

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    cart_id = current_cart.id
-    user = login(params[:email], params[:password])
-    if user
-      session[:cart_id] = cart_id
+    set_cart_and_user
+    if @user
+      session[:cart_id] = @cart_id
       redirect_to cart_path(current_cart), :notice => "Logged in!"
     else
       flash.now.notice = "Email or password was invalid"
       render :new
     end
+  end
+
+  def set_cart_and_user
+    @cart_id = current_cart.id
+    @user = login(params[:email], params[:password])
   end
 
   def destroy
