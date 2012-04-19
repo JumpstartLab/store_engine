3454c0f341e97796eef869fd7b52cc06c2ce4baa
Andrew Thal <andrew.thal@livingocial.com>
2012-04-19 12:28:56 -0400

    remove email confirmation statement

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -53,8 +53,7 @@ class OrdersController < ApplicationController
 
   def check_out
     link = "<a href=\"#{url_for(@order)}\">View Details</a>"
-    notice = "Thank you for purchasing.
-    An email confirmation is on its way. #{link}".squish.html_safe
+    notice = "Thank you for purchasing. #{link}".html_safe
     session[:previous_order_id] = session[:order_id] if !logged_in?
     session[:order_id] = nil
     redirect_to root_url, notice: notice
