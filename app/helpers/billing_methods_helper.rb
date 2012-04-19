1ddcd4d61c45a8e89e8ba3b535597b8c7be46eb7
Andrew Thal <andrew.thal@livingocial.com>
2012-04-19 09:13:41 -0400

    fix more styling

--- app/helpers/billing_methods_helper.rb
+++ app/helpers/billing_methods_helper.rb
@@ -60,7 +60,6 @@ module BillingMethodsHelper
   def validate_billing_user
     if @billing_method.user_id != current_user.id
       redirect_to root_url, notice: "Sorry, you are not allowed to view that."
-    else
     end
   end
 end
--- app/helpers/shipping_addresses_helper.rb
+++ app/helpers/shipping_addresses_helper.rb
@@ -2,7 +2,6 @@ module ShippingAddressesHelper
   def validate_shipping_user
     if @shipping_address.user_id != current_user.id
       redirect_to root_url, notice: "Sorry, you are not allowed to view that."
-    else
     end
   end
 end
