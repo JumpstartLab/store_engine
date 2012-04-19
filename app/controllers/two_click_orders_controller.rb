fe1bb55682329f378fd8c455f438bb3894bff577
Chris Anderson <christopher.anderson@gmail.com>
2012-04-19 09:00:36 -0400

    two click order modded

--- app/controllers/two_click_orders_controller.rb
+++ app/controllers/two_click_orders_controller.rb
@@ -3,20 +3,25 @@ class TwoClickOrdersController < ApplicationController
 
   def create
     if current_user.addresses.first && current_user.stripe_id
-      @order = Order.new
-      @order.user = current_user
-      @order.status = Status.new
-      @order.save
-      @order.address = current_user.addresses.first
-      cart = Cart.create
-      cart.add_or_increment_by_product(params[:product_id])
-      @order.add_order_items_from(cart)
+      create_order
+      add_to_cart
       if @order.save_with_payment
         @order.status.change
         redirect_to order_path(@order.id),
         :notice => "Transaction Successful" and return
       end
     end
+    load_and_redirect
+  end
+
+  def create_order
+    @order = Order.new
+    @order.user = current_user
+    @order.status = Status.new
+    @order.save
+  end
+
+  def load_and_redirect
     @cart.add_or_increment_by_product(params[:product_id])
     redirect_to '/cart',
     :alert => "You don't have a saved credit card.
@@ -24,23 +29,12 @@ class TwoClickOrdersController < ApplicationController
               and Instant Purchase will be available next time."
   end
 
-  # def new_order_creation
-  #   if current_user.addresses.first && current_user.stripe_id
-  #     @order = Order.new
-  #     @order.user = current_user
-  #     @order.status = Status.new
-  #     @order.save
-  #     @order.address = current_user.addresses.first
-  #     cart = Cart.create
-  #     cart.add_or_increment_by_product(params[:product_id])
-  #     @order.add_order_items_from(cart)
-  #     if @order.save_with_payment
-  #       @order.status.change
-  #       redirect_to order_path(@order.id),
-  #       :notice => "Transaction Successful" and return
-  #     end
-  #   end
-  # end
+  def add_to_cart
+    @order.address = current_user.addresses.first
+    cart = Cart.create
+    cart.add_or_increment_by_product(params[:product_id])
+    @order.add_order_items_from(cart)
+  end
 
   def show
     redirect_to '/',
