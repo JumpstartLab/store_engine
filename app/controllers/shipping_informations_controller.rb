5c5b3e2d032abc0bf8a42599a7e58248f44170ac
ADigitalNative <adigitalnative@gmail.com>
2012-04-19 10:36:28 -0400

    Cleaning up for cane

--- app/controllers/shipping_informations_controller.rb
+++ app/controllers/shipping_informations_controller.rb
@@ -1,13 +1,15 @@
 class ShippingInformationsController < ApplicationController
 
   def update
-    current_user.shipping_information.update_attributes(params[:shipping_information])
+    user_shipping = current_user.shipping_information
+    user_shipping.update_attributes(params[:shipping_information])
     flash[:success] = "Shipping information saved!"
     redirect_to_last_page
   end
 
   def create
-    @shipping_information = current_user.shipping_information.build(params[:shipping_information])
+    user_shipping = current_user.shipping_information
+    @shipping_information = user_shipping.build(params[:shipping_information])
     if @shipping_information.save
       flash[:success] = "Shipping information saved!"
       redirect_to_last_page
