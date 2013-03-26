c47f592d64d62403486de7c46c362ef792bd4b81
Raphael Weiner <raphael.weiner@gmail.com>
2013-03-26 16:32:16 -0600

    Admin dashboard displays orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,14 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_attached_file :image, styles: { medium: "400x400!", thumb: "200x200!" }
 
-  validates :title, presence: :true, uniqueness: { case_sensitive: false }
+  validates :title, presence: :true,
+                    uniqueness: { case_sensitive: false }
   validates :description, presence: :true
   validates :status, presence: :true,
                      inclusion: { in: %w(active retired) }
   validates :price, presence: :true,
-            format: { with: /^\d+??(?:\.\d{0,2})?$/ },
-            numericality: { greater_than: 0 }
+                    format: { with: /^\d+??(?:\.\d{0,2})?$/ },
+                    numericality: { greater_than: 0 }
 
   def self.apply_filter(category_id = nil)
     if category_id.nil?
