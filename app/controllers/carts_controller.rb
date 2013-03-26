69dbf25740d5f311e7115abfcdfd2bbdcb46d689
Laura Steadman <lauramsteadman@gmail.com>
2013-03-26 16:28:07 -0600

    can add items to cart from product page; can view shopping cart page whether have added items to it or not. CANNOT update or remove items from the cart.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,15 @@
 class CartsController < ApplicationController
   def show
-    @cart = Hash[session[:cart].map { |id, quantity| [Product.find(id), quantity] }]
+    @cart = if session[:cart]
+              Hash[session[:cart].map{|id, quantity| [Product.find(id), quantity]}]
+            else
+              session[:cart] = {}
+            end
   end
 
   def update
     id = params[:product_id]
     quantity = params[:quantity]
-
     if quantity
       session[:cart][id] = quantity
     else
--- app/helpers/cart_helper.rb
+++
@@ -1,8 +0,0 @@
-module CartHelper
-  def total
-  end
-
-  def subtotal
-    product.price * quantity
-  end
-end
