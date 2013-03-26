e62b76c976abbd89fa55b670fb946087be310315
Raphael Weiner <raphael.weiner@gmail.com>
2013-03-26 16:23:57 -0600

    Chelsea pairing session

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ActiveRecord::Base
   attr_accessible :status, :order_item_ids
   belongs_to :user
   has_many :order_items
+  has_many :products, through: :order_items
 
   validates :user, presence: true
   validates_associated :order_items
