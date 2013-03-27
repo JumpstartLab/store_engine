52b8320ddf7558fbc37c9736e4f016c357506061
Laura Steadman <lauramsteadman@gmail.com>
2013-03-27 14:43:28 -0600

    cart feature specs

--- spec/features/user_views_cart_spec.rb
+++ spec/features/user_views_cart_spec.rb
@@ -3,13 +3,33 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'the user cart view', type: :feature do
+  context "when there are no items in the cart" do
+    it 'displays a message that the cart is empty' do
+      visit '/cart'
+      expect(page).to have_content("empty")
+    end
+  end
 
-  # it 'shows the cart with items quantities and prices' do
-  #   p = Product.create(title: 'thing', description: 'lots of them', price: 2.99)
+  context "when there are items in the cart" do
+    before do
+      FactoryGirl.create(:product)
+      FactoryGirl.build(:product)
+      p1 = Product.first
+      visit product_path(p1)
+      click_button 'Add To Cart'
+      visit '/cart'
+    end
 
-  #   visit :update, :key => p.id
+    it 'shows the cart with items quantities and prices' do
+      expect(page).to have_content("Total")
+    end
 
-  #   visit '/cart'
-  #   expect(page).to include("Product")
-  # end
+    context "the user wants to empty the cart" do
+      it "gets emptied" do
+        click_button 'Empty Cart'
+        expect(page).to have_content("empty")
+      end
+    end
+  end
+
 end
