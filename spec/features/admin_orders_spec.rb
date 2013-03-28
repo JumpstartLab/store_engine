a8d8c50a63b924a6e2ac19214c5479a6b1d3fa50
Raphael Weiner <raphael.weiner@gmail.com>
2013-03-28 11:23:08 -0600

    Admin dashboard acceptance tests

--- spec/features/admin_orders_spec.rb
+++ spec/features/admin_orders_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe "admin dashboard" do
+
+  before(:each) do
+      @user = FactoryGirl.create(:user, email: 'wtfz')
+      FactoryGirl.create(:order, user: @user, status: 'paid')
+      FactoryGirl.create(:order, user: @user, status: 'paid')
+      FactoryGirl.create(:order, user: @user, status: 'returned')
+  end
+
   context "when an admin visits their dashboard" do
     it "should have a list of all orders and link to each" do
-      user = FactoryGirl.create(:user)
-      order1 = FactoryGirl.create(:order, user: user)
-      order2 = FactoryGirl.create(:order, user: user)
+      order1 = FactoryGirl.create(:order, user: @user)
+      order2 = FactoryGirl.create(:order, user: @user)
 
       visit '/admin/dashboard'
       expect(page).to have_xpath("//a[@href='#{admin_order_path(order1)}']")
@@ -13,11 +20,6 @@ describe "admin dashboard" do
     end
 
     it "should show a total number of orders by status" do
-      user = FactoryGirl.create(:user)
-      FactoryGirl.create(:order, user: user, status: 'paid')
-      FactoryGirl.create(:order, user: user, status: 'paid')
-      FactoryGirl.create(:order, user: user, status: 'returned')
-
       visit '/admin/dashboard'
       expect(page).to have_content('0 pending')
       expect(page).to have_content('2 paid')
@@ -26,44 +28,79 @@ describe "admin dashboard" do
       expect(page).to have_content('0 cancelled')
     end
 
-    xit "should allow for filtering by status" do
-      user = FactoryGirl.create(:user)
-      FactoryGirl.create(:order, user: user, status: 'paid')
-      FactoryGirl.create(:order, user: user, status: 'paid')
-      FactoryGirl.create(:order, user: user, status: 'returned')
-
+    it "should allow for filtering by status" do
       visit '/admin/dashboard'
-
+      click_link('paid')
+      expect(page).to have_css('tr', count: 2)
     end
 
     context "within an individual order" do
-      xit "displays order creation date and time" do
+      before(:each) do
+        @order = FactoryGirl.create(:order, user: @user)
+        @product = FactoryGirl.create(:product)
+        @order_item = FactoryGirl.create(:order_item, order: @order, product: @product)
+        visit admin_order_path(@order)
       end
 
-      xit "displays purchaser's full name and email address" do
+      it "displays order creation date and time" do
+        expect(page).to have_content(@order.created_at.to_s(:short))
+        expect(page).to have_content(@order.updated_at.to_s(:short))
       end
 
-      xit "displays each product of the order with associated data" do
-        # Name with link to product page
-        # Quantity
-        # Price
-        # Line item subtotal
+      it "displays purchaser's full name and email address" do
+        expect(page).to have_content(@user.email)
       end
 
-      xit "displays order total" do
+      it "displays each product of the order with associated data" do
+        expect(page).to have_content(@product.title)
+        expect(page).to have_link(@product.title)
+        expect(page).to have_xpath("//a[@href='#{product_path(@product)}']")
+        expect(page).to have_content(@order_item.quantity)
+        expect(page).to have_content(@order_item.unit_price)
+        expect(page).to have_content(@order_item.subtotal)
       end
 
-      xit "displays order status" do
+      it "displays order total" do
+        expect(page).to have_content(@order.total)
+      end
+
+      it "displays order status" do
+        expect(page).to have_content(@order.status)
       end
 
       context "can update the order by" do
-        xit "progressing status based on rules" do
-          # link to "cancel" individual orders which are currently "pending"
-          # link to "mark as returned" individual orders which are currently "shipped"
-          # link to "mark as shipped" individual orders which are currently "paid"
+        it "progressing status based on rules" do
+          expect(page).to have_button('cancel')
+
+          order = FactoryGirl.create(:order, user: @user, status: 'paid')
+          visit admin_order_path(order)
+          expect(page).to have_button('mark as shipped')
+
+          order = FactoryGirl.create(:order, user: @user, status: 'shipped')
+          visit admin_order_path(order)
+          expect(page).to have_button('mark as returned')
+
+          order = FactoryGirl.create(:order, user: @user, status: 'cancelled')
+          visit admin_order_path(order)
+          expect(page).to_not have_button('mark as returned')
+          expect(page).to_not have_button('mark as shipped')
+          expect(page).to_not have_button('cancel')
         end
 
-        xit "changing quantity or removing products ONLY when status pending or paid" do
+        it "changing quantity ONLY when status pending or paid" do
+          fill_in('admin_order_item_quantity', with: '10')
+          click_button('Update')
+          expect(page).to have_content(@order_item.unit_price * 10)
+
+          expect(page).to have_xpath("//input[@id='admin_order_item_quantity']")
+          click_button('cancel')
+          expect(page).to_not have_xpath("//input[@id='admin_order_item_quantity']")
+        end
+
+        it "removing product ONLY when status pending or paid" do
+          expect(page).to have_content(@product.title)
+          click_button('Delete')
+          expect(page).to_not have_content(@product.title)
         end
       end
     end
