4beb6804b243d6bfbf054096bf4500e952be2317
Raphael Weiner <raphael.weiner@gmail.com>
2013-03-31 01:09:57 -0600

    improve i18n, add flags, currency localized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_cart
+  helper_method :current_cart, :get_flag
   before_filter :get_locale
 
   def require_admin
@@ -29,5 +29,13 @@ class ApplicationController < ActionController::Base
 
   def get_locale
     I18n.locale = session[:i18n] || I18n.default_locale || :en
+  end
+
+  def get_flag
+    case session[:i18n]
+    when 'fr' then 'fr'
+    when 'cs' then 'cs'
+    else 'us'
+    end
   end
 end
